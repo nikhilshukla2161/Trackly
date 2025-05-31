cabee8f6724764a6de8e8af442beef3d8ebe4b80
Nikhil Shukla <nikhilshukla2161@gmail.com>
2025-05-31 09:59:41 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,14 @@ const Navbar = () => {
       <div className="my-5 p-3 border border-gray-200 rounded-md flex justify-between items-center">
         <span className="leading-none">
           <h1 className="text-2xl font-semibold leading-none bg-gradient-to-br from-gray-300 via-black text-transparent bg-clip-text">
-            PennWise
+            Trackly
           </h1>
-          <p className="text-xs">manage expense in clicks</p>
+          <p className="text-xs">Know Where Your Money Goes.</p>
         </span>
         <span>
-            <a href="https://github.com/nikhilshukla2161/Trackly">
+          <a href="https://github.com/nikhilshukla2161/Trackly">
             <FaGithub className="size-8 hover:text-gray-500" />
-            </a>
+          </a>
         </span>
       </div>
     </div>
